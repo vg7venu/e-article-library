7d40bffe3fbf4e9b50deef8b0d8636f73f673bfd
vg7venu <vg7venu@gmail.com>
2021-11-01 22:01:56 +0530

    Init project

---
+++ src/main/java/com/team_b/e_article_library/EArticleLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.team_b.e_article_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EArticleLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EArticleLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team_b/e_article_library/EArticleLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_b.e_article_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EArticleLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

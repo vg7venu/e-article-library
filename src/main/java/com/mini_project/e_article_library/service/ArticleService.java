32f3101a9efd8e9a21790925c93faa96e9d68a66
vg7venu <vg7venu@gmail.com>
2021-11-03 10:49:53 +0530

    Refactor the generate links

--- src/main/java/com/mini_project/e_article_library/service/ArticleService.java
+++ src/main/java/com/mini_project/e_article_library/service/ArticleService.java
@@ -1,6 +1,5 @@
 package com.mini_project.e_article_library.service;
 
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,19 +9,18 @@ import com.mini_project.e_article_library.repository.ArticleRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @Service
 public class ArticleService {
     @Autowired
     ArticleRepository articleRepository;
 
+    private final String BASEURL = "http://localhost:8080/v1/articles/";
+
     public List<String> generateArticleLinksByCategory(Optional<List<ArticleDto>> article) {
         List<String> listOfURLs = new ArrayList<String>();
         for (int i = 0; i < article.get().size(); i++) {
-            URI location = ServletUriComponentsBuilder.fromHttpUrl("http://localhost:8080/v1/articles").path("/{id}")
-                    .buildAndExpand(article.get().get(i).getId()).toUri();
-            listOfURLs.add(location.toString());
+            listOfURLs.add(BASEURL + article.get().get(i).getId());
         }
         return listOfURLs;
     }

6235ac69f9ffc2d78d10829279f47fb0e6e199d6
vg7venu <vg7venu@gmail.com>
2021-11-22 12:33:31 +0530

    Remove Stub

--- src/main/java/com/mini_project/e_article_library/security/MyUserDetails.java
+++ src/main/java/com/mini_project/e_article_library/security/MyUserDetails.java
@@ -15,8 +15,7 @@ import com.mini_project.e_article_library.jpa.model.User;
 public class MyUserDetails implements UserDetails {
 
 	private User user;
-	
-	
+
 	public MyUserDetails(User user) {
 		super();
 		this.user = user;
@@ -24,7 +23,6 @@ public class MyUserDetails implements UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
 		Set<Roles> roles = user.getRoles();
 		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
 		for (Roles role : roles) {
@@ -35,37 +33,31 @@ public class MyUserDetails implements UserDetails {
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
 		return user.getPassword();
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
 		return user.getUserName();
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
 		return user.isEnabled();
 	}
 
